1fbefa9fa98db2b0b030a19782295c698bf9f051
cxspace <442961832@qq.com>
2016-06-11 20:39:40 +0800

    Initial commit

---
+++ src/snakegame/Data.java
@@ -0,0 +1,26 @@
+package snakegame;
+
+/**
+ * Created by cxspace on 16-6-11.
+ */
+public interface Data {
+
+    //一个食物的宽和高都是15px
+    public  final  int LONG = 15;
+
+    //矩形的左右上下标志
+    public  final  int LEFT = 0;
+    public  final  int RIGHT = 19;
+    public  final  int UP = 0;
+    public  final  int DOWN = 19;
+
+    //定义上下左右的标志常量
+    public final int RIGHTFLAG = 1;
+    public  final  int DOWNFLAG = 2;
+    public  final  int LEFTFLAG = 3;
+    public  final  int UPFLAG = 4;
+
+    public final int ACTUALLEFT = 15;
+    public final  int ACTUALUP = 10;
+
+}
